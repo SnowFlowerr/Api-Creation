8fed71a7f89182a42b1ea7c4c48578810909adef
SnowFlowerr <bhudeokrit@gmail.com>
2024-12-22 20:45:42 +0530

    feat: Added readme file
    - updated readme file
    
    Signed-off-by: SnowFlowerr <bhudeokrit@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,12 +1,9 @@
 import express from "express";
-import xmlparser from 'express-xml-bodyparser';
 
 const port = 8080;
 
 const app = express();
-app.use(express.json());
 app.use(express.text());
-app.use(xmlparser());
 
 const router = express.Router();
 
@@ -17,19 +14,19 @@ let data = "";
 router.post("/", async function createUser(req, res) {
     try {
         data=req.body
-        return res.status(201).json( req.body );
+        return res.status(201).send( req.body );
     }
     catch (err) {
-        return res.status(500).json(err)
+        return res.status(500).send(err)
     }
 })
 
 router.get("/", async function getUser(req, res) {
     try {
-        return res.status(201).json(data);
+        return res.status(201).send(data);
     }
     catch (err) {
-        return res.status(500).json(err)
+        return res.status(500).send(err)
     }
 })
 
